44d5a22370bded3d1f6cf8308f05fcb37afdf84a
yulannn <145021271+yulannn@users.noreply.github.com>
2026-02-12 12:38:49 +0100

    test

--- backend/src/cocktail/cocktail.service.ts
+++ backend/src/cocktail/cocktail.service.ts
@@ -12,7 +12,7 @@ export class CocktailService {
     const cocktails = await this.prisma.cocktail.findMany();
 
     if (cocktails.length === 0) {
-      throw new NotFoundException('No cocktails founds');
+      throw new NotFoundException('No cocktails founnds');
     }
 
     return cocktails;
